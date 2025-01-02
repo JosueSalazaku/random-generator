5465608152aedc488c6f6b96bc79228761a68186
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-02 13:19:39 +0100

    Add navigation and random button to the main layout

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
 import './style.css'
 
+const cockTaialsSections = document.getElementById('cocktails-section') as HTMLElement;
+const randomBtn = document.getElementById("random-button") as HTMLButtonElement;
+
+
+
+randomBtn.addEventListener('click', () => {
+    console.log("Random")
+})
