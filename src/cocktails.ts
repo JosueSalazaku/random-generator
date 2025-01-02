e7f5dd6a4387d5e0a8ab16dfc1474d5bbf50a5b9
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-02 13:23:09 +0100

    Create cocktail menu/ data

---
+++ src/cocktails.ts
@@ -0,0 +1,108 @@
+export interface Cocktail {
+    name: string;
+    price: number;
+    baseDrink: string;
+}
+
+export const cocktailMenu: Cocktail[] = [
+    {
+        name: "Mojito",
+        price: 12,
+        baseDrink: "Rum"
+    },
+    {
+        name: "Margarita",
+        price: 10,
+        baseDrink: "Tequila"
+    },
+    {
+        name: "Old Fashioned",
+        price: 15,
+        baseDrink: "Whiskey"
+    },
+    {
+        name: "Martini",
+        price: 14,
+        baseDrink: "Gin"
+    },
+    {
+        name: "Pina Colada",
+        price: 13,
+        baseDrink: "Rum"
+    },
+    {
+        name: "Cosmopolitan",
+        price: 11,
+        baseDrink: "Vodka"
+    },
+    {
+        name: "Daiquiri",
+        price: 10,
+        baseDrink: "Rum"
+    },
+    {
+        name: "Negroni",
+        price: 13,
+        baseDrink: "Gin"
+    },
+    {
+        name: "Whiskey Sour",
+        price: 12,
+        baseDrink: "Whiskey"
+    },
+    {
+        name: "Mai Tai",
+        price: 14,
+        baseDrink: "Rum"
+    },
+    {
+        name: "Manhattan",
+        price: 15,
+        baseDrink: "Whiskey"
+    },
+    {
+        name: "Bloody Mary",
+        price: 12,
+        baseDrink: "Vodka"
+    },
+    {
+        name: "Tom Collins",
+        price: 11,
+        baseDrink: "Gin"
+    },
+    {
+        name: "Mint Julep",
+        price: 13,
+        baseDrink: "Bourbon"
+    },
+    {
+        name: "Sidecar",
+        price: 14,
+        baseDrink: "Cognac"
+    },
+    {
+        name: "Sazerac",
+        price: 15,
+        baseDrink: "Rye Whiskey"
+    },
+    {
+        name: "French 75",
+        price: 13,
+        baseDrink: "Gin"
+    },
+    {
+        name: "Pisco Sour",
+        price: 12,
+        baseDrink: "Pisco"
+    },
+    {
+        name: "Caipirinha",
+        price: 11,
+        baseDrink: "Cachaça"
+    },
+    {
+        name: "Aperol Spritz",
+        price: 10,
+        baseDrink: "Aperol"
+    }
+];
