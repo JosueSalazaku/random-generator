f95e99d98c64675405bff4434f1ed34b585e3aa3
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-02 13:46:16 +0100

    Refactor random cocktail selection to log a single random cocktail

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import './style.css'
 import { Cocktail } from './cocktails';
 import { cocktailMenu } from './cocktails';
 
-
 const cockTaialsSections = document.getElementById('cocktails-section') as HTMLElement;
 const randomBtn = document.getElementById("random-button") as HTMLButtonElement;
 
@@ -11,7 +10,6 @@ randomBtn.addEventListener('click', () => {
     for (let i = 0; i < cocktailMenu.length; i++) {
         randomCocktails.push(cocktailMenu[Math.floor(Math.random() * cocktailMenu.length)]);
     }
-    // return randomCocktails
-    console.log(randomCocktails);
-    
+    // return randomCocktails[0];
+    console.log(randomCocktails[0]);
 })
