933c5505444015158a4ee8b7ffa4a5d3bf4edc7f
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-02 13:41:44 +0100

    Implement random cocktail selection on button click

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import './style.css'
+import { Cocktail } from './cocktails';
+import { cocktailMenu } from './cocktails';
+
 
 const cockTaialsSections = document.getElementById('cocktails-section') as HTMLElement;
 const randomBtn = document.getElementById("random-button") as HTMLButtonElement;
 
-
-
 randomBtn.addEventListener('click', () => {
-    console.log("Random")
+    let randomCocktails: Cocktail[] = [];
+    for (let i = 0; i < cocktailMenu.length; i++) {
+        randomCocktails.push(cocktailMenu[Math.floor(Math.random() * cocktailMenu.length)]);
+    }
+    // return randomCocktails
+    console.log(randomCocktails);
+    
 })
