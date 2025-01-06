3bc144db5c75baa65a01cc08235f385a4d8374cf
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-06 19:19:30 +0100

    Refactor layout: adjust main structure and navigation component for improved alignment

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -43,11 +43,8 @@
   }
 </script>
 
-<main class="flex flex-col items-center justify-center min-h-screen px-4 text-white bg-violet-500">
-  <header>
-    <Nav />
-  </header>
-  
+<main class="flex flex-col items-center justify-start min-h-screen px-4 text-white bg-violet-500">
+  <Nav />
   <div class="flex flex-col items-center justify-center w-full gap-3 py-12">
     <h1 class="py-5 text-4xl font-bold text-center">Create a QR Code for Your Menu Randomizer</h1>
     <p class="text-lg leading-relaxed text-center">
--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -2,7 +2,7 @@
     import { link } from 'svelte-spa-router';
   </script>
   
-  <nav class="py-4 text-2xl font-bold">
+  <nav class="py-4 text-2xl font-bold ">
     <a use:link href="/">Menu Roulette</a>
   </nav>
   
