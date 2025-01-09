7337282c7a696719e317cb9c1cb7e2c2a8db2844
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-09 01:39:10 +0100

    Enhance file upload functionality: implement custom storage with diskStorage and dynamic filename generation

--- server/src/files/files.controller.ts
+++ server/src/files/files.controller.ts
@@ -7,13 +7,31 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
 import { Express } from 'express';
+import * as fs from 'fs';
+import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly fileService: FilesService) {}
 
   @Post('uploads')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: (req, file, callback) => {
+          const uploadDir = './uploads';
+          if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir);
+          }
+          callback(null, uploadDir);
+        },
+        filename: (req, file, callback) => {
+          const filename = `${Date.now()}-${file.originalname}`;
+          callback(null, filename);
+        },
+      }),
+    }),
+  )
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     return await this.fileService.uploadFile(file);
   }
--- server/src/files/files.service.ts
+++ server/src/files/files.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class FilesService {
   async uploadFile(file: Express.Multer.File) {
+    const filePath = `uploads/${file.filename}`;
     return {
       originalName: file.originalname,
       filename: file.filename,
       size: file.size,
       mimeType: file.mimetype,
+      path: filePath,
     };
   }
 }
