346ef0d3b581d9e0819d23b891d30915a6d78f98
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-09 01:20:33 +0100

    Add file upload interceptor to FilesController for handling file uploads

--- server/src/files/files.controller.ts
+++ server/src/files/files.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Post, UploadedFile } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { FilesService } from './files.service';
 import { Express } from 'express';
 
@@ -7,6 +13,7 @@ export class FilesController {
   constructor(private readonly fileService: FilesService) {}
 
   @Post('uploads')
+  @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
     return await this.fileService.uploadFile(file);
   }
