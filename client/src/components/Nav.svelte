0308a68fd76e55a733e02b438ad2c30eff2e93e6
Josué Salazaku <josuesalazaku@gmail.com>
2025-01-10 21:10:52 +0100

    Add lucide-svelte dependency and improve navigation layout

--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { link } from 'svelte-spa-router';
   import { onMount } from 'svelte';
+  import { X } from 'lucide-svelte';
+  import { AlignJustify } from 'lucide-svelte';
 
   let isMobile = false;
   let isOpen = false;
@@ -21,14 +23,17 @@
 </script>
 
 {#if isMobile}
-  <nav class="py-4 text-2xl font-bold ">
-    <a use:link href="/">BiteSpin</a>
-    <button on:click={handleDropDownCLick}>X</button>
+  <nav class="flex flex-row items-center justify-between w-screen px-4 py-4 text-2xl">
+    <a use:link href="/" class="font-bold">BiteSpin</a>
+    <button on:click={handleDropDownCLick}><AlignJustify size="30" /></button>
     
     {#if isOpen}
     <div class="fixed inset-0 z-50 flex flex-col items-center justify-center opacity-100 ju jus jush-screen bg-violet-800">
-      <button on:click={handleDropDownCLick}>x</button>
-      <div class="flex flex-col items-center justify-center gap-4">
+      <div class="absolute flex flex-row items-center justify-between w-screen px-4 top-4 ">
+        <a use:link href="/" class="font-bold">BiteSpin</a>
+        <button class="" on:click={handleDropDownCLick}><X size="30" /></button>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-4 text-4xl">
         <a on:click={handleDropDownCLick} use:link href="/">BiteSpin</a>
         <a on:click={handleDropDownCLick} use:link href="/about">About</a>
         <a on:click={handleDropDownCLick} use:link href="/login">Login/ Sign Up</a>
@@ -38,9 +43,11 @@
     
   </nav>
 {:else}
-  <nav class="py-4 text-2xl font-bold ">
-    <a use:link href="/">BiteSpin</a>
-    <a use:link href="/about">About</a>
-    <a use:link href="/login">Login/ Sign Up</a>
+  <nav class="flex justify-between w-screen gap-3 px-4 py-4 text-2xl">
+    <a use:link href="/"  class="font-bold">BiteSpin</a>
+    <div class="space-x-4">
+      <a use:link href="/about">About</a>
+      <a use:link href="/login">Login/ Sign Up</a>
+    </div>
   </nav>
 {/if}
